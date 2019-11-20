52c816237dcefae93f82d808532aada4084e9477
Wagner Silva <you@example.com>
2019-11-20 16:27:42 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TextCounter from './TextCounter';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TextCounter limit={20} />
+        <TextCounter />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/TextCounter.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class TextCounter extends Component {
+    static defaultProps = {
+        limit: 15
+    }
+
+    constructor(props){
+        super(props);
+        this.state = {
+            totalChars: 0,
+            text: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        const element = event.target,
+            text = element.value;
+
+        if( text.length <= this.props.limit){
+            this.setState({
+                totalChars: text.length,
+                text
+            });
+        }
+
+    }
+
+
+    render() {
+        const {state, props} = this;
+        return (
+            <div>
+                <h1>Meu contador</h1>
+                <textarea onChange={this.handleChange} value={ state.text } />
+                <div>
+                    <strong>Total: </strong> { this.state.totalChars } / { props.limit }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TextCounter;
